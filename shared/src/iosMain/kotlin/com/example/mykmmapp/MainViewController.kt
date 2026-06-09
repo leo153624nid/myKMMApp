95e146e65bac9724f32c4196c516c75bf7dad5f8
Aleksey Chaykin <barjastroy@gmail.com>
2026-06-09 15:21:33 +0300

    fix ios

--- shared/src/iosMain/kotlin/com/example/mykmmapp/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/example/mykmmapp/MainViewController.kt
@@ -1,5 +1,17 @@
 package com.example.mykmmapp
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.navigation.compose.rememberNavController
+import com.example.mykmmapp.navigation.AppNavHost
+import com.example.mykmmapp.navigation.AppNavigator
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val navController = rememberNavController()
+    val navigator = remember { AppNavigator() }
+
+    AppNavHost(
+        navHostController = navController,
+        navigator = navigator
+    )
+}
